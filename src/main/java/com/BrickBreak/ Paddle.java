78c47b0688f12426eb269a209dfd8ff2be698487
Ben32245 <benjaminkish44@gmail.com>
2026-01-20 14:39:17 +0000

    submission

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -1,20 +1,31 @@
+// January 2026
+// Benjamin Kish
+// Creates paddle in brickbreak
+
 package com.BrickBreak;
 
 import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
 import java.awt.*;
 
 public class Paddle {
 
 
 	//your code here!
-
+	int paddleX;
+	int paddleY;
 
 	
 	//don't forget you need instance variables:
-	
+	rect(0,50,25,75);
 	
 	//constructor(s):
-	
+	public static paddleX {
+	}
+	public static paddleY{
+	}
 	//methods:
 	
 	
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,15 +1,24 @@
+//January 2026
+//Benjamin Kish
+//Creates ball in brickbreak
+
 package com.BrickBreak;
 import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
+	int ballX;
+	int ballY;
+
 	//don't forget you need instance variables:
-	
+	rect(100,150,125,175);
 	
 	
 	//constructor(s):
-	
+	public static ballX {
+	}
+	public static ballY{
+	}
 	//methods:
 
 }
